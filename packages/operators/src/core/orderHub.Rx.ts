5d5f5bd58136c779a153b5d66cba1af9cb29b068
lei1996 <617218165@qq.com>
2022-06-19 16:58:49 +0000

    重写记录器.

--- packages/operators/src/core/orderHub.Rx.ts
+++ packages/operators/src/core/orderHub.Rx.ts
@@ -1,10 +1,8 @@
 import {
   Big,
-  BigSourceQueue,
   Observable,
   Subscriber,
 } from '@data-analysis/core';
-import { Order } from '@data-analysis/core/src/order';
 import { OrderElementInterface } from '@data-analysis/types/order.type';
 import { WareHouseRxInterface } from '../types/wareHouse.Rx';
 
@@ -12,34 +10,25 @@ export const orderHubRxOperator = (queueLength: number) => {
   return (observable: Observable<OrderElementInterface>) =>
     new Observable<WareHouseRxInterface>(
       (subscriber: Subscriber<WareHouseRxInterface>) => {
-        let order: Order = new Order(1);
-        let result: BigSourceQueue = new BigSourceQueue(queueLength);
+        let order = {
+          sum: new Big(0),
+          prev: new Big(0),
+        };
 
         const subscription = observable.subscribe({
           next(item) {
             const { _price, info } = item;
 
             if (info.includes('开')) {
-              order.open({
-                price: _price,
-                quantity: 1,
-              });
+              order.prev = new Big(_price);
             } else if (info.includes('平')) {
-              const avgPrice = order.avgPrice();
-
-              if (avgPrice.eq(0)) {
-                return;
-              }
-
-              order.close();
-
               const diffPrice =
                 info === '平空'
-                  ? new Big(_price).minus(avgPrice)
-                  : new Big(avgPrice).minus(new Big(_price));
+                  ? new Big(_price).minus(order.prev)
+                  : new Big(order.prev).minus(new Big(_price));
 
-              result.push(diffPrice);
-              subscriber.next({ sum: result.sum, lastDiff: diffPrice });
+              order.sum = order.sum.plus(diffPrice);
+              subscriber.next({ sum: order.sum, lastDiff: diffPrice });
             }
           },
           error(err) {
@@ -55,7 +44,6 @@ export const orderHubRxOperator = (queueLength: number) => {
           // console.log('wareHouseRx 清空所有operator状态');
           // Clean up all state.
           subscription.unsubscribe();
-          result = null!;
           order = null!;
         };
       },
