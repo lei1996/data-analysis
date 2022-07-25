7844c53580d17317a276fc5d31f1ab87c927e3b2
lei1996 <617218165@qq.com>
2022-07-25 14:23:18 +0000

    update

--- reloadHuobi.sh
+++ reloadHuobi.sh
@@ -1,4 +1,4 @@
 pm2 stop huobi
 echo '' > ~/.pm2/logs/huobi-out.log
 echo '' > ~/.pm2/logs/huobi-error.log
-pm2 restart huobi --cron-restart="4 6 */2 * *"
+pm2 restart huobi --cron-restart="4 0 */2 * *"
